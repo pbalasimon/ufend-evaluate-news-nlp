2e55c7e1ccc82e853718aef7a328982090c4ae47
Pedro Bala Simón <pbalasimon@gmail.com>
2020-07-19 18:28:42 +0200

    feat: Config styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,11 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+import './styles/index.scss';
+
 console.log(checkForName);
+
+
 
 alert("I EXIST")
 console.log("CHANGE!!");
