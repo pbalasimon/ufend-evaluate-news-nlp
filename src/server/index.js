93bad2f6fb7ff0a67b8ea4c920d067c4460f72dc
Pedro Bala Simón <pbalasimon@gmail.com>
2020-07-19 14:30:27 +0200

    fix: Fix bug setting up aylien api

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const aylien = require("aylien_textapi");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const textapi = new AYLIENTextAPI({
+const textapi = new aylien({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY,
 });
